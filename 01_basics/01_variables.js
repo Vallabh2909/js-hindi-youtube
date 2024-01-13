acbf004a166f0ceedf30d4de74bd2108c85002e6
Vallabh Wasule <120954910+Vallabh2909@users.noreply.github.com>
2024-01-13 17:44:09 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" // Can be possible but dont use it
 let accountState;
 
 // accountId = 2 // not allowed
